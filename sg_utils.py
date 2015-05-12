6df4dd1e3f2eac3e8c900a94c99c5ab3f4638ad1
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-05-12 00:45:09 -0700

    documentation

--- sg_utils.py
+++ sg_utils.py
@@ -5,15 +5,18 @@ import os
 from IPython.core.debugger import Tracer
 
 def mkdir_if_missing(output_dir):
+  """
+  def mkdir_if_missing(output_dir)
+  """
   if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 def save_variables(pickle_file_name, var, info, overwrite = False):
   """
-    def save_variables(pickle_file_name, var, info, overwrite = False):
+    def save_variables(pickle_file_name, var, info, overwrite = False)
   """
   if os.path.exists(pickle_file_name) and overwrite == False:
-    raise Exception('{:s} exists and over write is false'.format(pickle_file_name))
+    raise Exception('{:s} exists and over write is false.'.format(pickle_file_name))
   # Construct the dictionary
   assert(type(var) == list); assert(type(info) == list);
   d = {}
@@ -23,6 +26,11 @@ def save_variables(pickle_file_name, var, info, overwrite = False):
     cPickle.dump(d, f, cPickle.HIGHEST_PROTOCOL)
 
 def load_variables(pickle_file_name):
+  """
+  d = load_variables(pickle_file_name)
+  Output:
+    d     is a dictionary of variables stored in the pickle file.
+  """
   if os.path.exists(pickle_file_name):
     with open(pickle_file_name, 'rb') as f:
       d = cPickle.load(f)
