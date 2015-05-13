fea6843a6532988548712d5995767588db0e986a
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-05-13 00:56:50 -0700

    script for checking correctness of code against matlab code

---
+++ scripts/script_matlab.py
@@ -0,0 +1,23 @@
+## MATLAB vocabulary
+if job_name == 'vocab':
+  import csv
+  import sg_utils
+  matlab_vocab = 'cachedir/v1/vocab_words.txt';
+  words = []; poss = []; counts = [];
+  with open(matlab_vocab, 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+      words.append(row[0].strip()) 
+      poss.append(row[1].strip()) 
+      counts.append(int(row[2].strip()))
+  sg_utils.save_variables('cachedir/v1/vocab_train.pkl', [words, poss, counts], \
+    ['words', 'poss', 'counts'], overwrite = True) 
+
+## Load the detections
+if job_name == 'eval_det':
+  import sg_utils
+  import test_model
+  import cap_eval_utils
+  vocab = sg_utils.load_variables('cachedir/v1/vocab_train.pkl')
+  dt = sg_utils.scio.loadmat('cachedir/v1/gt_labels_val.all.mat'); labels = dt['labels'];
+  details = test_model.benchmark(None, vocab, labels, 5., 'cachedir/v1/mil_prob_val.all.mat')
