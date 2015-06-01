c60565e090e32d2713dbc4527858b5d7b7061a87
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-06-01 15:44:43 -0700

    ..

--- scripts/script_all.py
+++ scripts/script_all.py
@@ -8,7 +8,7 @@ python run_mil.py --task compute_targets \
 GLOG_logtostderr=1 caffe/build_mil_vader/tools/caffe.bin train -gpu 0 \
   -model output/v1/mil_finetune.prototxt \
   -solver output/v1/mil_finetune_solver.prototxt \
-  -weights caffe-data/vgg_16_full_conv.caffemodel 2>&1 \
+  -weights ../caffe-data/vgg_16_full_conv.caffemodel 2>&1 \
   | tee output/v1/training.log
 
 # Testing the model
@@ -21,12 +21,12 @@ python run_mil.py --task test_model \
 python run_mil.py --task eval_model --gpu 1 \ 
   --prototxt_deploy models/vgg/mil_finetune.prototxt.deploy \
   --model models/vgg/snapshot_iter_240000.caffemodel \
-  --vocab_file cachedir/v1/vocab_train.pkl 
+  --vocab_file ../cachedir/v1/vocab_train.pkl 
   --test_set valid1
 
 # Generating output txt files
 python run_mil.py --task output_words --gpu 1 \
-  --model models/vgg/snapshot_iter_240000.caffemodel \
+  --model output/vgg/snapshot_iter_240000.caffemodel \
   --test_set valid1 \
   --calibration_set valid1 \
   --vocab_file cachedir/v1/vocab_train.pkl 
