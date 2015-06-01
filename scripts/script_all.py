7fa1b4d86185bbf4b682f7d2850710fbea473bca
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-06-01 16:31:22 -0700

    tweaks..

--- scripts/script_all.py
+++ scripts/script_all.py
@@ -1,32 +1,34 @@
-# Write splits and labels for training to be used for Caffe
+# Create directory, write splits and labels for training to be used for Caffe
+mkdir output/v1
+
 python run_mil.py --task compute_targets \
   --train_dir output/v1/ --write_labels 1 --write_splits 1 \
   --train_set train --val_set valid1 \
-  --vocab_file cachedir/v1/vocab_train.pkl 
+  --vocab_file vocabs/vocab_train.pkl 
 
-# Launch the training
+# Command to launch the training
 GLOG_logtostderr=1 caffe/build_mil_vader/tools/caffe.bin train -gpu 0 \
   -model output/v1/mil_finetune.prototxt \
   -solver output/v1/mil_finetune_solver.prototxt \
   -weights ../caffe-data/vgg_16_full_conv.caffemodel 2>&1 \
   | tee output/v1/training.log
 
-# Testing the model
+# Testing the pre-trained model
 python run_mil.py --task test_model \
-  --prototxt_deploy models/vgg/mil_finetune.prototxt.deploy \
-  --model models/vgg/snapshot_iter_240000.caffemodel \
+  --prototxt_deploy output/vgg/mil_finetune.prototxt.deploy \
+  --model output/vgg/snapshot_iter_240000.caffemodel \
   --test_set valid1 --gpu 1
 
-# Benchmarking the model
+# Benchmarking the pre-trained model
 python run_mil.py --task eval_model --gpu 1 \ 
-  --prototxt_deploy models/vgg/mil_finetune.prototxt.deploy \
-  --model models/vgg/snapshot_iter_240000.caffemodel \
-  --vocab_file ../cachedir/v1/vocab_train.pkl 
+  --prototxt_deploy output/vgg/mil_finetune.prototxt.deploy \
+  --model output/vgg/snapshot_iter_240000.caffemodel \
+  --vocab_file vocabs/vocab_train.pkl 
   --test_set valid1
 
-# Generating output txt files
+# Generating output txt files for pre-trained model
 python run_mil.py --task output_words --gpu 1 \
   --model output/vgg/snapshot_iter_240000.caffemodel \
   --test_set valid1 \
   --calibration_set valid1 \
-  --vocab_file cachedir/v1/vocab_train.pkl 
+  --vocab_file vocabs/vocab_train.pkl 
--- scripts/script_matlab.py
+++ scripts/script_matlab.py
@@ -2,7 +2,7 @@
 if job_name == 'vocab':
   import csv
   import sg_utils
-  matlab_vocab = 'cachedir/v1/vocab_words.txt';
+  matlab_vocab = 'vocabs/vocab_words.txt';
   words = []; poss = []; counts = [];
   with open(matlab_vocab, 'rb') as f:
     reader = csv.reader(f)
@@ -10,14 +10,15 @@ if job_name == 'vocab':
       words.append(row[0].strip()) 
       poss.append(row[1].strip()) 
       counts.append(int(row[2].strip()))
-  sg_utils.save_variables('cachedir/v1/vocab_train.pkl', [words, poss, counts], \
+  sg_utils.save_variables('vocabs/vocab_train.pkl', [words, poss, counts], \
     ['words', 'poss', 'counts'], overwrite = True) 
 
 ## Load the detections
-if job_name == 'eval_det':
-  import sg_utils
-  import test_model
-  import cap_eval_utils
-  vocab = sg_utils.load_variables('cachedir/v1/vocab_train.pkl')
-  dt = sg_utils.scio.loadmat('cachedir/v1/gt_labels_val.all.mat'); labels = dt['labels'];
-  details = test_model.benchmark(None, vocab, labels, 5., 'cachedir/v1/mil_prob_val.all.mat')
+# Code to re-evaluate matlab output, to check sainty of python code
+# if job_name == 'eval_det':
+#   import sg_utils
+#   import test_model
+#   import cap_eval_utils
+#   vocab = sg_utils.load_variables('cachedir/v1/vocab_train.pkl')
+#   dt = sg_utils.scio.loadmat('cachedir/v1/gt_labels_val.all.mat'); labels = dt['labels'];
+#   details = test_model.benchmark(None, vocab, labels, 5., 'cachedir/v1/mil_prob_val.all.mat')
